726e2ae856109d07113c2efb99454b7f2cd93acf
mohsen <mohsen.bernaoui@gmail.com>
2023-07-10 21:00:24 +0100

    need to fix even listener to only take input one button each time

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function playerSelection() {
+function playerSelection(string) {
 
-    let test = prompt(
-        "choose paper, rock or scissor",
-        "paper"
-    ).toLocaleLowerCase();
+    let test = string;
 
-    while (check(test) === false) {
-        test = prompt(
-            "choose one of these options: paper, rock or scissor"
-        ).toLocaleLowerCase();
-    }
-    return (player_choix = test);
+    // while (check(test) === false) {
+    //     test = prompt(
+    //         "choose one of these options: paper, rock or scissor"
+    //     ).toLocaleLowerCase();
+    // }
+    // return (player_choix = test);
+    return test;
+    // take input from the player then check it
 }
 
-function check(test) {
+// function check(test) {
 
-    let choice = ["paper", "rock", "scissor"];
-    let exit = false;
+//     let choice = ["paper", "rock", "scissor"];
+//     let exit = false;
     
-    for (let i = 0; i <= test.length; i++) {
-        if (choice[i] === test) {
-            exit = true;
-            break;
-        }
-    }
-    return exit;
-}
+//     for (let i = 0; i <= test.length; i++) {
+//         if (choice[i] === test) {
+//             exit = true;
+//             break;
+//         }
+//     }
+//     // check if the player only paper rock or scissors
+//     return exit;
+// }
 
 function computerSelection() {
     const comptS = Math.floor(Math.random() * 3) + 1;
@@ -36,14 +36,19 @@ function computerSelection() {
     } else {
         return "scissor";
     }
+    // take random choice from the computer
 }
 
-function PlayRound() {
+function PlayRound(string) {
+    console.log(string);
 
-    player = playerSelection();
+    player = playerSelection(string);
     compt = computerSelection();
 
-    return GameLogic(player, compt);
+    
+    return GameLogic(player,compt);
+    
+    // take input from the player and the computer and put them in vars
 }
 
 function GameLogic(player, compt) {
@@ -79,24 +84,29 @@ function GameLogic(player, compt) {
     }
 
     console.log(compt);
+    // game logic that will decide the winner for only one round
 }
 
-function game() {
+function game(string) {
     let play_w = 0;
     let compt_w = 0;
     while (play_w !== 5 && compt_w !== 5) {
-        if (PlayRound() === "player") {
+        if (PlayRound(string) === "player") {
             play_w++;
         } else {
             compt_w++;
         }
-    }
+    } 
     let rest=[play_w,compt_w];
+    
+    console.log("player= "+play_w ,'\n',"computer= ",compt_w);
+    
     return rest;
+    // play 5 rounds and take result each round until one of them wins
 }
 
-function result(){
-    let rest=game();
+function result(string){
+    let rest=game(string);
     let winner;
     if (rest[0]>rest[1]){
         winner='player won !'
@@ -104,6 +114,16 @@ function result(){
     if (rest[0]<rest[1]){
         winner="computer won !"
     }
-    return winner;
+    return alert(winner);
+    // decide who is the winner 
 }
-alert(result())
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+      result(button.id);
+    });
+  });
+  
